543586b22b71163fdd6ac42834a28566ac92899c
litianyi <sky2017lty@gmail.com>
2021-08-04 09:58:50 +0800

    Initial commit

---
+++ src/main/java/com/poshing/checkdemo/CheckDemoApplication.java
@@ -0,0 +1,13 @@
+package com.poshing.checkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poshing/checkdemo/CheckDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poshing.checkdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
